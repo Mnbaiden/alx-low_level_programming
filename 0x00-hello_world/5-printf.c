20f069c5bae8aed0ae8b03a1b09ff4c0c94cb28f
mnbaiden <mickbaiden@gmail.com>
2023-07-15 06:08:58 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main
+ * main - point of entry
  *
  * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
