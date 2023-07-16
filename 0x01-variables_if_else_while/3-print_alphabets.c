548753998463758aec8f36485baded27f81c74c5
mnbaiden <mickbaiden@gmail.com>
2023-07-16 13:19:27 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
