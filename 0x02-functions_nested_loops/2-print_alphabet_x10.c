1758692bfe90b1a745e5319271207dbdcb83732d
mnbaiden <mickbaiden@gmail.com>
2023-07-18 07:37:15 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print lowercase alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char num, letter;
+
+	for (letter = 0; num <= 'z'; letter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar('\n');
+		}
+	}
+}
