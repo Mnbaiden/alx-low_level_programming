cd5f5189e282babc132081653be7b6567235af2e
mnbaiden <mickbaiden@gmail.com>
2023-07-20 06:13:15 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - func checks for a digit (0 through).
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
