5bc608aed82ec9dd8e63300f75144f861b902797
mnbaiden <mickbaiden@gmail.com>
2023-07-27 08:54:42 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * it will use at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+
