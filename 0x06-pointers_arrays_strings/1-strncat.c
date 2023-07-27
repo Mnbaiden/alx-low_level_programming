5954253aa9aae98c6063fcb889c625de704f0d97
mnbaiden <mickbaiden@gmail.com>
2023-07-27 09:02:17 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,4 +8,23 @@
  * @n: input value
  * Return: dest
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
 
+	i = 0;
+	while (dest[i] != '\n')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
