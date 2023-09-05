a27f3a1b847186dd7091a8ac7e146d487b053fa0
mnbaiden <mickbaiden@gmail.com>
2023-09-05 07:51:22 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written = 0;
+	int fd, bytes_written = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[bytes_written])
-            bytes_written++;
-        if (write(fd, text_content, bytes_written) == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
-
-    close(fd);
-    return (1);
+	if (text_content != NULL)
+	{
+		while (text_content[bytes_written])
+			bytes_written++;
+		if (write(fd, text_content, bytes_written) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
 }
